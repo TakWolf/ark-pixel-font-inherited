ba1cd319dd44c8e9a647c42478c0cffb3c2ea5a5
TakWolf <takwolf@foxmail.com>
2023-02-15 16:48:11 +0800

    添加模版配置

--- configs/__init__.py
+++ configs/__init__.py
@@ -2,6 +2,8 @@ import os
 import random
 from types import SimpleNamespace
 
+from jinja2 import Environment, FileSystemLoader
+
 from configs import path_define
 from utils.unidata_util import UnidataDB
 
@@ -14,3 +16,5 @@ ark_pixel_config = SimpleNamespace(
 )
 
 unidata_db = UnidataDB(os.path.join(path_define.unidata_dir, 'Blocks.txt'))
+
+template_env = Environment(loader=FileSystemLoader(path_define.templates_dir))
