00ca2e2bfe19ee4595a21417318e8034787561e7
TakWolf <takwolf@foxmail.com>
2023-02-15 16:51:09 +0800

    添加 git 部署配置

--- configs/__init__.py
+++ configs/__init__.py
@@ -5,6 +5,7 @@ from types import SimpleNamespace
 from jinja2 import Environment, FileSystemLoader
 
 from configs import path_define
+from configs.git_deploy_config import GitDeployConfig
 from utils.unidata_util import UnidataDB
 
 build_random_key = random.random()
@@ -18,3 +19,9 @@ ark_pixel_config = SimpleNamespace(
 unidata_db = UnidataDB(os.path.join(path_define.unidata_dir, 'Blocks.txt'))
 
 template_env = Environment(loader=FileSystemLoader(path_define.templates_dir))
+
+git_deploy_configs = [GitDeployConfig(
+    'git@github.com:TakWolf/ark-pixel-font-inherited.git',
+    'github',
+    'gh-pages',
+)]
