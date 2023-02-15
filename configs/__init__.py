3de43a030fafedbecd7037ac4a5bdc18b97fee09
TakWolf <takwolf@foxmail.com>
2023-02-15 19:15:16 +0800

    字体配置

--- configs/__init__.py
+++ configs/__init__.py
@@ -4,7 +4,8 @@ from types import SimpleNamespace
 
 from jinja2 import Environment, FileSystemLoader
 
-from configs import path_define
+from configs import path_define, font_config
+from configs.font_config import FontConfig
 from configs.git_deploy_config import GitDeployConfig
 from utils.unidata_util import UnidataDB
 
@@ -15,6 +16,25 @@ ark_pixel_config = SimpleNamespace(
     source_type='branch',
     source_name='master',
 )
+
+font_name = font_config.display_name_prefix
+font_version = font_config.version
+
+font_configs = [FontConfig(px) for px in [10, 12, 16]]
+font_config_map = {font_config.px: font_config for font_config in font_configs}
+
+width_modes = [
+    'monospaced',
+    'proportional',
+]
+
+width_mode_dir_names = [
+    'common',
+    'monospaced',
+    'proportional',
+]
+
+font_formats = ['otf', 'woff2', 'ttf']
 
 unidata_db = UnidataDB(os.path.join(path_define.unidata_dir, 'Blocks.txt'))
 
---
+++ configs/font_config.py
@@ -0,0 +1,111 @@
+import os
+import time
+import tomllib
+
+from configs import path_define
+
+display_name_prefix = 'Ark Pixel Inherited'
+unique_name_prefix = 'Ark-Pixel-Inherited'
+output_name_prefix = 'ark-pixel-inherited'
+style_name = 'Regular'
+version = f'{time.strftime("%Y.%m.%d")}'
+copyright_string = 'Copyright (c) 2023, TakWolf (https://takwolf.com), with Reserved Font Name "Ark Pixel Inherited".'
+designer = 'TakWolf'
+description = 'Open source Pan-CJK pixel font.'
+vendor_url = 'https://ark-pixel-font-inherited.takwolf.com'
+designer_url = 'https://takwolf.com'
+license_description = 'This Font Software is licensed under the SIL Open Font License, Version 1.1.'
+license_info_url = 'https://scripts.sil.org/OFL'
+
+
+class FontAttrs:
+    def __init__(self, config_data):
+        self.box_origin_y_px = config_data['box_origin_y_px']
+        self.x_height_px = config_data['x_height_px']
+        self.cap_height_px = config_data['cap_height_px']
+
+
+class VerticalMetrics:
+    def __init__(self, ascent, descent, x_height, cap_height):
+        self.ascent = ascent
+        self.descent = descent
+        self.x_height = x_height
+        self.cap_height = cap_height
+
+
+class FontConfig:
+    def __init__(self, px, px_units=100):
+        self.px = px
+
+        config_file_path = os.path.join(path_define.ark_pixel_glyphs_dir, str(px), 'config.toml')
+        if not os.path.exists(config_file_path):
+            return
+        with open(config_file_path, 'rb') as config_file:
+            config_data = tomllib.load(config_file)['font']
+        assert px == config_data['px'], config_file_path
+
+        self.line_height_px = config_data['line_height_px']
+        assert (self.line_height_px - px) % 2 == 0, f'font_config {px}px with incorrect line_height_px {self.line_height_px}px'
+        self.monospaced_attrs = FontAttrs(config_data['monospaced'])
+        self.proportional_attrs = FontAttrs(config_data['proportional'])
+        self.px_units = px_units
+
+        self.demo_html_file_name = f'demo-{px}px.html'
+        self.preview_image_file_name = f'preview-{px}px.png'
+
+    def get_name_strings(self, width_mode):
+        display_name = f'{display_name_prefix} {self.px}px {width_mode}'
+        unique_name = f'{unique_name_prefix}-{self.px}px-{width_mode}-{style_name}'
+        return {
+            'copyright': copyright_string,
+            'familyName': display_name,
+            'styleName': style_name,
+            'uniqueFontIdentifier': f'{unique_name};{version}',
+            'fullName': display_name,
+            'version': version,
+            'psName': unique_name,
+            'designer': designer,
+            'description': description,
+            'vendorURL': vendor_url,
+            'designerURL': designer_url,
+            'licenseDescription': license_description,
+            'licenseInfoURL': license_info_url,
+        }
+
+    def get_units_per_em(self):
+        return self.px * self.px_units
+
+    def get_box_origin_y(self, width_mode):
+        if width_mode == 'monospaced':
+            attrs = self.monospaced_attrs
+        else:  # proportional
+            attrs = self.proportional_attrs
+        return attrs.box_origin_y_px * self.px_units
+
+    def get_vertical_metrics(self, width_mode):
+        if width_mode == 'monospaced':
+            line_height_px = self.px
+            attrs = self.monospaced_attrs
+        else:  # proportional
+            line_height_px = self.line_height_px
+            attrs = self.proportional_attrs
+        ascent = (attrs.box_origin_y_px + int((line_height_px - self.px) / 2)) * self.px_units
+        descent = ascent - line_height_px * self.px_units
+        x_height = attrs.x_height_px * self.px_units
+        cap_height = attrs.cap_height_px * self.px_units
+        return VerticalMetrics(ascent, descent, x_height, cap_height)
+
+    def get_font_file_name(self, width_mode, font_format):
+        return f'{output_name_prefix}-{self.px}px-{width_mode}.{font_format}'
+
+    def get_info_file_name(self, width_mode):
+        return f'font-info-{self.px}px-{width_mode}.md'
+
+    def get_alphabet_txt_file_name(self, width_mode):
+        return f'alphabet-{self.px}px-{width_mode}.txt'
+
+    def get_release_zip_file_name(self, width_mode, font_format):
+        return f'{output_name_prefix}-font-{self.px}px-{width_mode}-{font_format}-v{version}.zip'
+
+    def get_alphabet_html_file_name(self, width_mode):
+        return f'alphabet-{self.px}px-{width_mode}.html'
