3baa75cadb67925035c2e07a857e33c7ef8b91c6
TakWolf <takwolf@foxmail.com>
2023-05-18 18:45:02 +0800

    清理无效路径

--- configs/path_define.py
+++ configs/path_define.py
@@ -3,7 +3,6 @@ import os
 project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 assets_dir = os.path.join(project_root_dir, 'assets')
-unidata_dir = os.path.join(assets_dir, 'unidata')
 glyphs_dir = os.path.join(assets_dir, 'glyphs')
 ark_pixel_glyphs_dir = os.path.join(glyphs_dir, 'ark-pixel')
 patch_glyphs_dir = os.path.join(glyphs_dir, 'patch')
