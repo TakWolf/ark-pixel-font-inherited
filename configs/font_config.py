22e84f2fe9c60a03bfb4203690f0f387ad978cfe
TakWolf <takwolf@foxmail.com>
2023-06-05 17:37:36 +0800

    调整 FontConfig 结构

--- configs/font_config.py
+++ configs/font_config.py
@@ -6,8 +6,11 @@ from configs import path_define
 
 
 class FontAttrs:
-    def __init__(self, config_data):
+    def __init__(self, config_data, px, line_height_px):
+        self.line_height_px = line_height_px
         self.box_origin_y_px = config_data['box_origin_y']
+        self.ascent_px = self.box_origin_y_px + int((line_height_px - px) / 2)
+        self.descent_px = self.ascent_px - line_height_px
         self.x_height_px = config_data['x_height']
         self.cap_height_px = config_data['cap_height']
 
@@ -33,23 +36,32 @@ class FontConfig:
     LICENSE_URL = 'https://scripts.sil.org/OFL'
 
     def __init__(self, px, px_to_units=100):
-        self.px = px
+        self.ark_pixel_glyphs_dir = os.path.join(path_define.ark_pixel_glyphs_dir, str(px))
+        self.patch_glyphs_dir = os.path.join(path_define.patch_glyphs_dir, str(px))
 
         config_file_path = os.path.join(path_define.ark_pixel_glyphs_dir, str(px), 'config.toml')
         if not os.path.exists(config_file_path):
             return
-        with open(config_file_path, 'rb') as config_file:
-            config_data = tomllib.load(config_file)['font']
-        assert px == config_data['size'], config_file_path
+        with open(config_file_path, 'rb') as file:
+            config_data = tomllib.load(file)['font']
 
+        self.px = config_data['size']
+        assert self.px == px, f'Font config size not equals: expect {px} but actually {self.px}'
         self.line_height_px = config_data['line_height']
-        assert (self.line_height_px - px) % 2 == 0, f'font_config {px}px with incorrect line_height_px {self.line_height_px}px'
-        self.monospaced_attrs = FontAttrs(config_data['monospaced'])
-        self.proportional_attrs = FontAttrs(config_data['proportional'])
+        assert (self.line_height_px - self.px) % 2 == 0, f"Font config {self.px}: the difference between 'line_height' and 'size' must be a multiple of 2"
+
+        self._attrs_group = {
+            'monospaced': FontAttrs(config_data['monospaced'], self.px, self.px),
+            'proportional': FontAttrs(config_data['proportional'], self.px, self.line_height_px),
+        }
+
         self.px_to_units = px_to_units
 
         self.demo_html_file_name = f'demo-{px}px.html'
         self.preview_image_file_name = f'preview-{px}px.png'
+
+    def get_attrs(self, width_mode):
+        return self._attrs_group[width_mode]
 
     def get_name_strings(self, width_mode):
         style_name = 'Regular'
@@ -75,21 +87,12 @@ class FontConfig:
         return self.px * self.px_to_units
 
     def get_box_origin_y(self, width_mode):
-        if width_mode == 'monospaced':
-            attrs = self.monospaced_attrs
-        else:  # proportional
-            attrs = self.proportional_attrs
-        return attrs.box_origin_y_px * self.px_to_units
+        return self.get_attrs(width_mode).box_origin_y_px * self.px_to_units
 
     def get_vertical_metrics(self, width_mode):
-        if width_mode == 'monospaced':
-            line_height_px = self.px
-            attrs = self.monospaced_attrs
-        else:  # proportional
-            line_height_px = self.line_height_px
-            attrs = self.proportional_attrs
-        ascent = (attrs.box_origin_y_px + int((line_height_px - self.px) / 2)) * self.px_to_units
-        descent = ascent - line_height_px * self.px_to_units
+        attrs = self.get_attrs(width_mode)
+        ascent = attrs.ascent_px * self.px_to_units
+        descent = ascent - attrs.line_height_px * self.px_to_units
         x_height = attrs.x_height_px * self.px_to_units
         cap_height = attrs.cap_height_px * self.px_to_units
         return VerticalMetrics(ascent, descent, x_height, cap_height)
