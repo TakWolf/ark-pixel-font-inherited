5587358813a3da6a5cfe8c0c9e7c49366299eeb8
TakWolf <takwolf@foxmail.com>
2023-08-04 14:43:13 +0800

    Pillow 修复枚举引用

--- services/image_service.py
+++ services/image_service.py
@@ -56,7 +56,7 @@ def make_preview_image_file(font_config: FontConfig):
     _draw_text(image, (font_config.size, font_config.size + font_config.line_height * 6), 'the quick brown fox jumps over a lazy dog.', font)
     _draw_text(image, (font_config.size, font_config.size + font_config.line_height * 7), '0123456789', font)
     _draw_text(image, (font_config.size, font_config.size + font_config.line_height * 8), '★☆☺☹♠♡♢♣♤♥♦♧☀☼♩♪♫♬☂☁⚓✈⚔☯', font)
-    image = image.resize((image.width * 2, image.height * 2), Image.NEAREST)
+    image = image.resize((image.width * 2, image.height * 2), Image.Resampling.NEAREST)
 
     fs_util.make_dirs(path_define.outputs_dir)
     file_path = os.path.join(path_define.outputs_dir, font_config.preview_image_file_name)
