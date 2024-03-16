8500895883cf57a81c9dbb012e6d138129e68170
TakWolf <takwolf@foxmail.com>
2024-03-17 06:14:27 +0800

    调整清理逻辑

--- build.py
+++ build.py
@@ -5,7 +5,8 @@ from scripts.utils import fs_util
 
 
 def main():
-    fs_util.delete_dir(path_define.build_dir)
+    fs_util.delete_dir(path_define.outputs_dir)
+    fs_util.delete_dir(path_define.releases_dir)
 
     update_service.setup_glyphs()
 
