2b32fa51f8790033615af3b7ab6785fcfdfde6a1
TakWolf <takwolf@foxmail.com>
2024-03-17 23:42:23 +0800

    调整 ark_pixel_config

--- scripts/configs/__init__.py
+++ scripts/configs/__init__.py
@@ -1,5 +1,6 @@
 from scripts.configs.font import FontConfig
 from scripts.configs.deploy import GitDeployConfig
+from scripts.configs.source import GithubSourceConfig, GitSourceType
 
 width_modes = [
     'monospaced',
@@ -11,6 +12,12 @@ font_sizes = [10, 12, 16]
 font_configs = FontConfig.load_all()
 
 font_formats = ['otf', 'woff2', 'ttf', 'bdf']
+
+ark_pixel_config = GithubSourceConfig(
+    repository_name='TakWolf/ark-pixel-font',
+    source_type=GitSourceType.TAG,
+    source_name=None,
+)
 
 git_deploy_config = GitDeployConfig(
     url='git@github.com:TakWolf/ark-pixel-font-inherited.git',
