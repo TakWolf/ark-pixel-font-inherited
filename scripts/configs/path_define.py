ea046d83b96d18eed52f4e29063d74800a671303
TakWolf <takwolf@foxmail.com>
2024-03-20 14:57:04 +0800

    调整缓存目录位置

--- scripts/configs/path_define.py
+++ scripts/configs/path_define.py
@@ -8,9 +8,8 @@ ark_pixel_glyphs_dir = os.path.join(glyphs_dir, 'ark-pixel')
 templates_dir = os.path.join(assets_dir, 'templates')
 www_static_dir = os.path.join(assets_dir, 'www-static')
 
-cache_dir = os.path.join(project_root_dir, 'cache')
-
 build_dir = os.path.join(project_root_dir, 'build')
+cache_dir = os.path.join(build_dir, 'cache')
 outputs_dir = os.path.join(build_dir, 'outputs')
 releases_dir = os.path.join(build_dir, 'releases')
 www_dir = os.path.join(build_dir, 'www')
