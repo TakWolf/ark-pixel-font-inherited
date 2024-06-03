aedd2590d13f51ddf58a85b566c287393232c042
TakWolf <takwolf@foxmail.com>
2024-06-03 15:30:09 +0800

    调整 make_release_zips() 参数

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def main():
     update_service.setup_glyphs()
 
     font_configs = FontConfig.load_all()
-    for font_config in font_configs.values():
+    for font_size, font_config in font_configs.items():
         design_context = DesignContext.load(font_config)
         for width_mode in configs.width_modes:
             font_context = FontContext(design_context, width_mode)
@@ -21,7 +21,7 @@ def main():
             font_context.make_ttf()
             font_context.make_bdf()
             font_context.make_pcf()
-            publish_service.make_release_zips(font_config, width_mode)
+            publish_service.make_release_zips(font_size, width_mode)
             info_service.make_info_file(design_context, width_mode)
             info_service.make_alphabet_txt_file(design_context, width_mode)
             template_service.make_alphabet_html_file(design_context, width_mode)
--- scripts/tasks/release_16px_monospaced.py
+++ scripts/tasks/release_16px_monospaced.py
@@ -4,19 +4,20 @@ from scripts.services.font_service import DesignContext, FontContext
 
 
 def main():
+    font_size = 16
+    width_mode = 'monospaced'
+
     update_service.setup_glyphs()
 
-    font_config = FontConfig.load(16)
+    font_config = FontConfig.load(font_size)
     design_context = DesignContext.load(font_config)
-
-    width_mode = 'monospaced'
     font_context = FontContext(design_context, width_mode)
     font_context.make_otf()
     font_context.make_woff2()
     font_context.make_ttf()
     font_context.make_bdf()
     font_context.make_pcf()
-    publish_service.make_release_zips(font_config, width_mode)
+    publish_service.make_release_zips(font_size, width_mode)
     info_service.make_info_file(design_context, width_mode)
     info_service.make_alphabet_txt_file(design_context, width_mode)
 
