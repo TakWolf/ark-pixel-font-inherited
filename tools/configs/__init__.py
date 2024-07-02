cc54ad12ed348ff0f05beb852ffc8a5bc7802741
TakWolf <takwolf@foxmail.com>
2024-07-02 17:41:34 +0800

    代码整理

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -4,12 +4,12 @@ font_version = '2024.05.12'
 
 font_sizes = [10, 12, 16]
 
-font_formats = ['otf', 'woff2', 'ttf', 'bdf', 'pcf']
-
 width_modes = [
     'monospaced',
     'proportional',
 ]
+
+font_formats = ['otf', 'woff2', 'ttf', 'bdf', 'pcf']
 
 ark_pixel_config = GithubSourceConfig(
     repository_name='TakWolf/ark-pixel-font',
