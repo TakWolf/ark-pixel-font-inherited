37a913331794a8c72336c9c166760a1efd806966
TakWolf <takwolf@foxmail.com>
2024-07-02 19:42:32 +0800

    优化 inherited-mapping.yaml

--- tools/format_inherited_mapping.py
+++ tools/format_inherited_mapping.py
@@ -1,23 +1,20 @@
-from io import StringIO
-
 from tools.configs import path_define
 from tools.utils import fs_util
 
 
 def main():
     file_path = path_define.assets_dir.joinpath('inherited-mapping.yaml')
-
     mapping: dict[int, list[int]] = fs_util.read_yaml(file_path)
-    output = StringIO()
-    for target, code_points in sorted(mapping.items()):
-        code_points = sorted(set(code_points))
-        assert target in code_points
-        output.write('\n')
-        output.write(f'0x{target:04X}:  # {chr(target)}\n')
-        for code_point in code_points:
-            output.write(f'  - 0x{code_point:04X}  # {chr(code_point)}\n')
 
-    file_path.write_text(output.getvalue(), 'utf-8')
+    with file_path.open('w', encoding='utf-8') as file:
+        for target, code_points in sorted(mapping.items()):
+            code_points = set(code_points)
+            if target in code_points:
+                code_points.remove(target)
+            file.write('\n')
+            file.write(f'0x{target:04X}:  # {chr(target)}\n')
+            for code_point in sorted(code_points):
+                file.write(f'  - 0x{code_point:04X}  # {chr(code_point)}\n')
 
 
 if __name__ == '__main__':
