3fdb21cb8ef04487d1d10bdedc6d6a09bef4114c
TakWolf <takwolf@foxmail.com>
2024-07-03 15:32:25 +0800

    调整缓存目录配置

--- tools/configs/path_define.py
+++ tools/configs/path_define.py
@@ -5,9 +5,11 @@ project_root_dir = Path(__file__).parent.joinpath('..', '..').resolve()
 assets_dir = project_root_dir.joinpath('assets')
 templates_dir = assets_dir.joinpath('templates')
 
+cache_dir = project_root_dir.joinpath('cache')
+downloads_dir = cache_dir.joinpath('downloads')
+glyphs_dir = cache_dir.joinpath('glyphs')
+
 build_dir = project_root_dir.joinpath('build')
-cache_dir = build_dir.joinpath('cache')
-glyphs_dir = build_dir.joinpath('glyphs')
 outputs_dir = build_dir.joinpath('outputs')
 releases_dir = build_dir.joinpath('releases')
 
--- tools/services/update_service.py
+++ tools/services/update_service.py
@@ -39,38 +39,38 @@ def update_glyphs_version():
 
 
 def setup_glyphs():
-    build_version_file_path = path_define.glyphs_dir.joinpath('version.json')
-    if build_version_file_path.is_file():
-        build_sha = fs_util.read_json(build_version_file_path)['sha']
+    cache_version_file_path = path_define.glyphs_dir.joinpath('version.json')
+    if cache_version_file_path.is_file():
+        cache_sha = fs_util.read_json(cache_version_file_path)['sha']
     else:
-        build_sha = None
+        cache_sha = None
 
     version_file_path = path_define.assets_dir.joinpath('glyphs-version.json')
     version_info = fs_util.read_json(version_file_path)
     sha = version_info['sha']
-    if build_sha == sha:
+    if cache_sha == sha:
         return
     logger.info('Need setup glyphs')
 
-    download_dir = path_define.cache_dir.joinpath('ark-pixel-font')
-    source_file_path = download_dir.joinpath(f'{sha}.zip')
+    downloads_dir = path_define.downloads_dir.joinpath('ark-pixel-font')
+    source_file_path = downloads_dir.joinpath(f'{sha}.zip')
     if not source_file_path.exists():
         asset_url = version_info['asset_url']
         logger.info("Start download: '%s'", asset_url)
-        download_dir.mkdir(parents=True, exist_ok=True)
+        downloads_dir.mkdir(parents=True, exist_ok=True)
         download_util.download_file(asset_url, source_file_path)
     else:
         logger.info("Already downloaded: '%s'", source_file_path)
 
-    source_unzip_dir = download_dir.joinpath(f'ark-pixel-font-{sha}')
+    source_unzip_dir = downloads_dir.joinpath(f'ark-pixel-font-{sha}')
     fs_util.delete_dir(source_unzip_dir)
     with zipfile.ZipFile(source_file_path) as file:
-        file.extractall(download_dir)
+        file.extractall(downloads_dir)
     logger.info("Unzip: '%s'", source_unzip_dir)
 
     fs_util.delete_dir(path_define.glyphs_dir)
     source_glyphs_dir = source_unzip_dir.joinpath('assets', 'glyphs')
     source_glyphs_dir.rename(path_define.glyphs_dir)
     fs_util.delete_dir(source_unzip_dir)
-    fs_util.write_json(version_info, build_version_file_path)
+    fs_util.write_json(version_info, cache_version_file_path)
     logger.info("Update glyphs: '%s'", sha)
