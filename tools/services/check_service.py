b34cb914363978fdc9ca1f280b530e4122d1aabf
TakWolf <takwolf@foxmail.com>
2024-07-10 15:00:04 +0800

    调整错误文案

--- tools/services/check_service.py
+++ tools/services/check_service.py
@@ -7,4 +7,4 @@ from tools.services import format_service
 def check_inherited_mapping():
     file_path = path_define.assets_dir.joinpath('inherited-mapping.yml')
     text = file_path.read_text('utf-8')
-    assert text == format_service.dump_inherited_mapping(yaml.safe_load(text)), f"Unformatted 'inherited-mapping.yml'"
+    assert text == format_service.dump_inherited_mapping(yaml.safe_load(text)), f"unformatted 'inherited-mapping.yml'"
