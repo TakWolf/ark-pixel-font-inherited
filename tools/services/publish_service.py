63afa09062af0ebbf050144adcaba646c6fab19a
TakWolf <takwolf@foxmail.com>
2024-07-11 10:51:40 +0800

    命名调整

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -10,7 +10,7 @@ from tools.configs import path_define, FontSize, WidthMode, FontFormat
 
 def make_release_zip(font_size: FontSize, width_mode: WidthMode, font_format: FontFormat):
     path_define.releases_dir.mkdir(parents=True, exist_ok=True)
-    file_path = path_define.releases_dir.joinpath(f'ark-pixel-font-inherited-{font_size}px-{width_mode}-{font_format}-v{configs.font_version}.zip')
+    file_path = path_define.releases_dir.joinpath(f'ark-pixel-font-inherited-{font_size}px-{width_mode}-{font_format}-v{configs.version}.zip')
     with zipfile.ZipFile(file_path, 'w') as file:
         file.write(path_define.project_root_dir.joinpath('LICENSE-OFL'), 'OFL.txt')
         font_file_name = f'ark-pixel-inherited-{font_size}px-{width_mode}.{font_format}'
