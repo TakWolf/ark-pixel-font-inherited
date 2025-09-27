506af20e15158e1edaf24abed67f0747c70b60de
TakWolf <takwolf@foxmail.com>
2025-09-28 03:30:16 +0800

    调整映射文件配置

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -9,5 +9,6 @@ mapping_file_paths = [
     path_define.ark_pixel_mappings_dir.joinpath('2700-27BF Dingbats.yml'),
     path_define.ark_pixel_mappings_dir.joinpath('2E80-2EFF CJK Radicals Supplement.yml'),
     path_define.ark_pixel_mappings_dir.joinpath('2F00-2FDF Kangxi Radicals.yml'),
+    path_define.ark_pixel_mappings_dir.joinpath('1F100-1F1FF Enclosed Alphanumeric Supplement.yml'),
     path_define.mappings_dir.joinpath('Inherited.yml'),
 ]
